fb9b2909ca4599a513151d0d71957b12fa0f48dc
deaver812 <32045954+deaver812@users.noreply.github.com>
2023-01-01 12:52:31 -0500

    switched the loop to get broken as opposed to pointless run varibale

--- runicChat.py
+++ runicChat.py
@@ -8,14 +8,13 @@ number="0123456789"
 #settings for the OSC Client
 
 client = SimpleUDPClient("127.0.0.1", 6000) #create the OSC client
-run=True
 
 #This was orginally going to be apart of a OSC Library which is why client is an input
 #but decided until I actually have enough stuff to justify a entire VRC-OSC libray that this will just sit here
 def sendChatbox(client, text, bypass):
     client.send_message("/chatbox/input", [text, bypass])
 
-while(run==True): #the running loop
+while(True): #the running loop
     print("----------------------------------")
     print("type (ext) to exit the program")
     userInput= input("please enter what you wish to say: ")
@@ -23,7 +22,7 @@ while(run==True): #the running loop
     #this if, else statement purely exists for the purpose of adding an in-program way to exit the loop
     if userInput=="ext":
         print("Exiting program")
-        run=False
+        break
     else:
         #taking user input converting it to runes and sending it to VRChat
         #first in runic form then in english
